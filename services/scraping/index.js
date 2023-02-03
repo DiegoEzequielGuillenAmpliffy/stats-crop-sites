01c6de49d4c318094aee999993954695f8e169e8
DeGsoft <diegoezequielguillen@gmail.com>
2023-02-03 16:45:29 -0300

    [dev] feat (index.test): click button

--- services/scraping/index.js
+++ services/scraping/index.js
@@ -3,8 +3,18 @@ const { chromium } = require('playwright');
 require('dotenv').config();
 const URI = process.env.URI;
 
+// const getPage = async () => {
+//   browser = await chromium.launch({
+//     headless: true, args: ['--no-sandbox']
+//   });
+//   const context = await browser.newContext();
+//   return await context.newPage();
+// };
+const data = [];
+
 async function scrape (uri) {
   if (!uri) uri = URI;
+  console.log(uri)
   const browser = await chromium.launch({
     headless: true, args: ['--no-sandbox']
   });
@@ -12,14 +22,36 @@ async function scrape (uri) {
   const page = await context.newPage();
   await page.goto(uri);
   await page.waitForLoadState('networkidle');
+  // console.log(page)
   // const html = await page.content();
   // const fs = require('fs');
   // fs.appendFile('page.html', html, function (err) {
   //   if (err) throw err;
   //   console.log('Saved!');
   // });
-  const data = [];
-  const items = await page.$$('.tr');
+  /*
+  <a class="page-link" href="/websites/top-sites/2000/" aria-label="Next">Next
+  <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z">
+  </path> </svg></a>
+  */
+  // await page.click('a.page-link');
+  /*
+  <button class="btn" role="button" title="See more"><label>See more</label></button>
+  */
+  // await page.waitForTimeout(1000);
+  // await page.click('button.btn:text("See more")');
+  // await page.locator('button:text("See more")').click();
+  let length = 0;
+  let items;
+  while (length < 1000) {
+    await page.click('button.btn');
+    await page.waitForLoadState('networkidle');
+    items = await page.$$('.tr');
+    length = items.length;
+    console.log(length);
+  }
+
   for (const item of items) {
     const textSuccess = await item.$eval('.text-success', el => el.textContent);
     const site = textSuccess.trim();
@@ -30,10 +62,18 @@ async function scrape (uri) {
     data.push([site, '#' + rank, visitors]);
   }
   await page.waitForTimeout(1000);
+
   await browser.close();
   return data;
 }
 
+async function multiScrape (uri, number) {
+  if (!uri) uri = URI;
+  return await scrape(uri + '/' + number * 1 + '/');
+  // return await scrape(uri + '/' + number * 2 + '/');
+}
+
 module.exports = {
-  scrape
+  scrape,
+  multiScrape
 };
--- test/services/scraping/index.test.js
+++ test/services/scraping/index.test.js
@@ -1,7 +1,9 @@
-const { scrape } = require('../../../services/scraping');
+const { multiScrape, scrape } = require('../../../services/scraping');
 
 async function main () {
-  const data = await scrape();
+  const number = 1000;
+  // const data = await multiScrape('', number);
+  const data = await scrape('https://www.statscrop.com/websites/top-sites/1000/');
   console.log(data)
   return data;
 }
