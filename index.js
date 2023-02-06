1b1c025d215627e15f4f1e9dd3b2b3c3afa18aa5
DeGsoft <diegoezequielguillen@gmail.com>
2023-02-06 18:28:00 -0300

    [dev] feat (express): endpoints

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+const express = require('express');
+// const cors = require('cors');
+require('dotenv').config();
+
+const PORT = 3001;
+
+const app = express();
+// app.use(cors({origin: 'http://localhost:3000'}));
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { multiScrape } = require('./services/scraping');
 const { appendSpreadSheetValues } = require('./services/google-sheets');
 
-async function main () {
-  const number = 1000;
-  const data = await multiScrape('', number);
-  return await appendSpreadSheetValues(data);
-}
-main();
+app.get('/', async (req, res) => {
+  const { num, key } = req.query;
+  if (key === process.env.SPREAD_SHEET_ID) {
+    const data = await multiScrape('', num);
+    res.send(data);
+  }
+  res.send('clave inválida');
+});
 
-module.exports = { main };
+app.get('/append', async (req, res) => {
+  const { num, key } = req.query;
+  if (key === process.env.SPREAD_SHEET_ID) {
+    const data = await multiScrape('', num);
+    await appendSpreadSheetValues(data);
+    res.send(data);
+  }
+  res.send('clave inválida');
+});
+
+app.listen(PORT, () => console.log(`Node server listening on port ${PORT}!`));
